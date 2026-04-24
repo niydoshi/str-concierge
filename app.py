71422871454061e7ff9218e67a89a657bcb7f9a8
Niyati Doshi <nddoshi2@gmail.com>
2026-04-24 19:12:40 -0400

    Add root_path parameter to demo.launch

--- app.py
+++ app.py
@@ -73,4 +73,8 @@ with gr.Blocks(title="STR Concierge") as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+    demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860)),
+    root_path="/"
+)
