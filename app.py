98341807b7ec1de03b38d9eb2c45b0578aeab015
Niyati Doshi <nddoshi2@gmail.com>
2026-04-24 18:56:40 -0400

    Launch demo on specified server and port

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ with gr.Blocks(title="STR Concierge", theme=gr.themes.Soft()) as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
